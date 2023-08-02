5563eeeb2bf155311561032a69beff232276c99b
chrysanthemum <asingiser@gmail.com>
2023-08-02 11:28:16 -0400

    change text

--- src/App.js
+++ src/App.js
@@ -1,75 +1,73 @@
 import React from 'react';
-import { Box, CssBaseline, TextField , Button } from '@material-ui/core'
+import { Box, CssBaseline, TextField , Button } from '@material-ui/core';
 import emailjs from '@emailjs/browser';
 import Swal from 'sweetalert2';
 import { styles } from './styles';
 
 export default function App(){
+  const sendEmail = (e) => {
 
-    const sendEmail= (e) => {
-        e.preventDefault();
-    
-        emailjs.sendForm('service_ip0sttu', 'template_u09d72k', e.target, 'epwEA6jtM-fzbhBRM')
-          .then((result) => {
-            console.log(result.text);
-            Swal.fire({
-              icon:'success',
-              title:'Message sent sucessfully'
-            })
-          }, (error) => {
-            console.log(error.text);
-            Swal.fire({
-              icon:'error',
-              title:'Something went wrong',
-              text:error.text,
-            })
-          });
-          e.target.reset();
-      };
+    e.preventDefault();
+
+    emailjs.sendForm('service_ip0sttu', 'template_u09d72k', e.target, 'epwEA6jtM-fzbhBRM')
+      .then((result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: 'success',
+          title: 'Message sent sucessfully'
+        })
+      }, (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: 'error',
+          title: 'Something went wrong',
+          text: error.text,
+        })
+      });
+    e.target.reset();
+};
 
   return (
     <>
-    <CssBaseline />
+      <CssBaseline />
       <Box style={styles.loginscreen}>
-        <form  onSubmit={sendEmail}>
-            <Box>
-                 <h2>Contact Form</h2>
-                <TextField fullWidth style={styles.text}
-                  type="text"
-                  label="Name"
-                  required
-                  name="user_name"
-                  variant="outlined"
-                />
-            </Box>
-            <Box>
-                <TextField fullWidth style={styles.text}
-                    type="email"
-                    label="Email"
-                    name="user_email"
-                    required
-                    variant="outlined"
-                />
-            </Box>
-
-            <Box >
-                <TextField 
-                    multiline
-                    style={styles.message}
-                    type="text"
-                    label="Message"
-                    required
-                    name="user_message"
-                    variant="outlined"
-                    placeholder='Message here...'
-                />
-            </Box>
-
-            <Box style={styles.btnLogin}>
-                <Button type='submit' style={styles.btnText}>SUBMIT MESSAGE</Button>
-            </Box> 
+        <form onSubmit={sendEmail}>
+          <Box>
+            <h2>Sign Up For My Newsletter !</h2>
+            <TextField fullWidth style={styles.text}
+              type="text"
+              label="Name"
+              required
+              name="user_name"
+              variant="outlined"
+            />
+          </Box>
+          <Box>
+            <TextField fullWidth style={styles.text}
+              type="email"
+              label="Email"
+              name="user_email"
+              required
+              variant="outlined"
+            />
+          </Box>
+          <Box >
+            <TextField
+              multiline
+              style={styles.message}
+              type="text"
+              label="Message"
+              required
+              name="user_message"
+              variant="outlined"
+              placeholder='Message here...'
+            />
+          </Box>
+          <Box style={styles.btnLogin}>
+            <Button type='submit' style={styles.btnText}>SUBMIT MESSAGE</Button>
+          </Box>
         </form>
-    </Box>
+      </Box>
     </>
   )
 }
